d9c81267deeb18a55a67d79a5ce0e9413503363c
Tadeo Luffi <131204961+TadeoLuffi@users.noreply.github.com>
2023-08-15 17:51:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -30,7 +30,50 @@ const listadoCartas = [
         imagen: "https://aica.org/imagenes/noticias/messi-y-el-mayor-de-sus-trofeos-la-familia-kHhI.jpg",
         descripcion: "Messi con la familia",
     },
+    {
+        id: 6,
+        nombre: "Messi Friends",
+        imagen: "https://media.gettyimages.com/id/502056676/es/foto/lionel-messi-neymar-and-luis-suarez-of-barcelona-hold-the-winners-trophy-after-the-fifa-club.jpg?s=612x612&w=gi&k=20&c=mwMxxOlX6Rhv0TmmVCYBJB-9JpO-5eMZSkBvK4fre3A=",
+        descripcion: "Messi con los amigos",
+    },
+    {
+        id: 7,
+        nombre: "Messi Sleeping",
+        imagen: "https://www.ole.com.ar/2022/12/21/41skvoT5x_720x0__1.jpg",
+        descripcion: "Messi Durmiendo",
+    },
+    {
+        id: 8,
+        nombre: "Messi Chill",
+        imagen: "https://s.yimg.com/ny/api/res/1.2/1Sb3zmbq2AyyiWwLi5jOjA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MA--/https://media.zenfs.com/es/lanacion.com.ar/7a02765ee3beb1eb80e82e0458a59a60",
+        descripcion: "Messi Relajdado",
+    },
+    {
+        id: 9,
+        nombre: "Messi with Kun ",
+        imagen: "https://tn.com.ar/resizer/W3iuUM9DA_BYxj5-nnSp9U_mMMw=/767x0/smart/filters:format(webp)/cloudfront-us-east-1.images.arcpublishing.com/artear/J36XKBS2EBAVDGFQDBHFV5UHVY.jfif",
+        descripcion: "Messi con el Kun ",
+    },
+    {
+        id: 10,
+        nombre: "Messi with Maradona ",
+        imagen: "https://img2.rtve.es/i/?w=1600&i=1606329475622.jpg",
+        descripcion: "Messi con Maradona ",
+    },
+    {
+        id: 11,
+        nombre: "Messi  ",
+        imagen: "https://w0.peakpx.com/wallpaper/4/105/HD-wallpaper-messi-argentina-2021-messi-2021-messi-new-2021-messi-2021-messi-argentina-messi-new-messi-argentina-thumbnail.jpg",
+        descripcion: "Messi Fachero ",
+    },
+    {
+        id: 12,
+        nombre: "Messi con Sus trofeos",
+        imagen: "https://pbs.twimg.com/media/FxKb2lTWIAIV4tj.jpg:large",
+        descripcion: "Messi Campeon de todo",
+    },
 ]
+ 
 
 
 /* Selección de los NODOS HTML (etiquetas/elementos html) */
@@ -38,6 +81,10 @@ const contenedorCartas = document.querySelector(".listadoCartas")
 
 
 /* Función que recorre el listado de las cartas */
+function recorrerCartas() {
+    
+}
+
 listadoCartas.forEach(
     (carta) => contenedorCartas.innerHTML += ` <article class="card">
     <img src=${carta.imagen}>
@@ -47,6 +94,15 @@ listadoCartas.forEach(
     </div>
     </article> `
 )
+function agregarCarta() {
+   const nombre = prompt("Nombre")
+   const imagen = prompt("Imagen")
+   const descripcion = prompt("Descripcion")
+
+   const carta = {nombre,imagen,descripcion}
+
+   listadoCartas.push(Carta)
+}
 
 
 
